cc1bebc0e5325738f4bd93a46d33db2a9dce8313
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-10-16 01:23:12 +0100

    js

--- scripts.js
+++ scripts.js
@@ -2,18 +2,14 @@ var btn = document.querySelector("button");
 
 btn.addEventListener('click', function (){
 
-    document.body.classList.toggle("light-theme");
-    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
 
-    var txtbtn = document.body.className;
-
-    if(txtbtn == "light-theme")
-    {
-        this.textContent = "Light";
-    }
-    else
-    {
+    var className = document.body.className;
+    if(className == "light-theme") {
         this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
     }
 });
 
